230cc0115dea1a7f865ded1047511bca651752d7
1JzzQAQ <138588862+1JzzQAQ@users.noreply.github.com>
2025-06-08 11:03:39 +0800

    db set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const font = Nunito({
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body
           className={`${font.variable}`}
         >
+          <Toaster />
           {children}
         </body>
       </html>
